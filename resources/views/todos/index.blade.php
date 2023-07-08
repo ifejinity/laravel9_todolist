06b04ce881e2f12a90d73819d1be94d70099d84f
ifejinity <mrlonzanida08@gmail.com>
2023-07-08 17:21:48 +0800

    1st commit

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -18,7 +18,7 @@
                 <div class="card-body">
                 <div class="card-actions justify-between">
                     <p class="text-[24px] text-indigo-600 font-bold">ADD</p>
-                    <button class="btn btn-square btn-sm" id="modalClose">
+                    <button type="button" class="btn btn-square btn-sm" id="modalClose">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
                     </button>
                 </div>
